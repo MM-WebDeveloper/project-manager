62170ac110b6adaa55147b03926ce62bb90d3587
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-10-19 21:42:13 +0200

    added connection to mongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
+const colors = require('colors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
 
 app.use(
 	'/graphql',
