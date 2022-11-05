56e0679b89f79ecb141a73e3dcacf891d542098e
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-11-05 21:27:24 +0100

    added the add client mutation

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,10 +1,10 @@
-const { projects, clients } = require('../sampleData.js');
 const {
 	GraphQLObjectType,
 	GraphQLID,
 	GraphQLString,
 	GraphQLSchema,
 	GraphQLList,
+	GraphQLNonNull,
 } = require('graphql');
 
 const Project = require('../models/Project');
@@ -70,6 +70,31 @@ const RootQuery = new GraphQLObjectType({
 	},
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addClient: {
+			type: ClientType,
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				email: { type: GraphQLNonNull(GraphQLString) },
+				phone: { type: GraphQLNonNull(GraphQLString) },
+			},
+			resolve(parent, args) {
+				const client = new Client({
+					name: args.name,
+					email: args.email,
+					phone: args.phone,
+				});
+
+				return client.save();
+			},
+		},
+	},
+});
+
 module.exports = new GraphQLSchema({
 	query: RootQuery,
+	mutation,
 });
